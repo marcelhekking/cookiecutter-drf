ce27a3225cc0a4832b5f5c39c9d034d463f73869
Marcel Hekking <marcelhekking@gmail.com>
2025-04-03 07:34:53 +0200

    Start Nginx in a container rather than the one on the host
    makes running the application via Docker `out-of-the-box`

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,8 +11,8 @@ POSTGRES_USER=postgres
 POSTGRES_PASSWORD=a_secret_password
 
 WEB_IMAGE={{cookiecutter.project_slug}}-web:latest
-MEDIAFILES_HOST=~/www/omni/public/mediafiles/
-STATICFILES_HOST=~/www/omni/public/staticfiles/
+MEDIAFILES_HOST=current_working_dir/public/mediafiles/
+STATICFILES_HOST=current_working_dir/public/staticfiles/
 """
 
 # Define the path to the file where the original working directory is stored
