432aa40c41e3aebbf25bd8329adae5a9c7a5997f
Marcel Hekking <marcelhekking@gmail.com>
2025-04-23 21:25:02 +0200

    Remove obsolete endpoint

--- {{cookiecutter.project_slug}}/backend/src/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/backend/src/{{cookiecutter.project_slug}}/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path, reverse_lazy
 from django.views.generic.base import RedirectView
-from rest_framework.authtoken import views
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.routers import DefaultRouter
 
@@ -14,15 +13,10 @@ router = DefaultRouter()
 router.register(r"users", UserViewSet)
 
 
-class ObtainAuthTokenOwn(ObtainAuthToken):
-    authentication_classes = ()
-
-
 urlpatterns = [
     path("get-token/", ObtainAuthToken.as_view(), name="get-token"),
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
-    path("api-token-auth/", views.obtain_auth_token),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
