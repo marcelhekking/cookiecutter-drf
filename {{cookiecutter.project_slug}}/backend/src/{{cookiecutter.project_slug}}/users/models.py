25e11d9c571dc994fe8a017e0a9c8d0e6e21332d
Marcel Hekking <marcelhekking@gmail.com>
2025-04-24 13:32:31 +0200

    Revert "Fix migration plus change model"
    This reverts commit aedb34f90eb35569e621b043e71b10eec4cfbe20.

--- {{cookiecutter.project_slug}}/backend/src/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/backend/src/{{cookiecutter.project_slug}}/users/models.py
@@ -9,6 +9,8 @@ from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     def __str__(self):
         return self.username
 
